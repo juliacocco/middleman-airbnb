72779b6a01d4c884c1d186889a06fe4ab2416b2b
Julia Cocco <juliatcocco@gmail.com>
2018-11-12 17:35:47 +0100

    Added CSS for colors and fonts to over-ride Bootstrap theme

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+data.flats.each do |owner|
+  proxy "/flats/#{owner.first}.html", "/flats/show.html", locals: { owner: owner.first }, ignore: true
+end
